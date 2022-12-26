901496e0f2e10ba338a17aa4b3b0f27d216b2666
Jay Salot <jay.salot@bitontree.com>
2022-12-26 11:40:37 +0530

    Form builder

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const FormObject = (type: string) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <input
+      type={type}
+      id="email_1"
+      name="email_1"
+      data-alias=""
+      className="form-control"
+    />
+  );
+};
+
+const object = [
+  {
+    type: "email",
+    name: "email_1",
+    id: "email_1",
+  },
+  { type: "password", name: "password1", id: "password1" },
+];
+export default function App({ isDragging, text }: any) {
+  return (
+    <>
+      <form id="form-app">
+        <fieldset className="row">
+          <div className="col-xs-12">
+            <h3 className="legend">Untitled Form</h3>
+          </div>
+          <div className="col-xs-12">
+            <p>This is my form. Please fill it out. Thanks!</p>
+          </div>
+            <div className="col-xs-12">
+            <div className="form-group">
+              <label className="control-label" htmlFor="email_1">
+                Email Field
+              </label>
+              {object.map((index) => {
+               return FormObject(index.type)
+              })}
+            </div>
+          </div>
+          <div className="col-xs-12">
+            <p>You can edit this paragraph by clicking here.</p>
+          </div>
+          <div className="col-xs-12">
+            <div className="form-action">
+              <button
+                type="submit"
+                id="button_1"
+                name="button_1"
+                className="btn btn-primary"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </fieldset>
+      </form>
+    </>
   );
 }
-
-export default App;
