0d797c43104fc282dd8f3aa20760de2e0fd8844f
karan4610 <karanprajapati9428733581@gmail.com>
2022-12-29 11:38:52 +0530

    task/mui-integrated/ui-improved

--- src/drop.tsx
+++ src/drop.tsx
@@ -1,26 +1,92 @@
-import React, { useState } from "react";
+import { Formik } from "formik";
+import React, { useMemo, useState } from "react";
 import { useDrop } from "react-dnd";
 import { Card } from "./card";
 import "./index.scss";
+import LabelAndFeilds from "./labelAndFeilds";
+import { Box, Button } from "@mui/material";
+
+const inputObject: any = {
+  email: (
+    index: number,
+    label: string,
+    handleChange: any,
+    handleBlur: any,
+    values: any,
+    errors: any,
+    touched: any,
+    ) => (
+   <LabelAndFeilds index={index} label={label} handleChange={handleChange} handleBlur={handleBlur} values={values} errors={errors} touched={touched} type="email"/>
+   ),
+  password: (
+    index: number,
+    label: string,
+    handleChange: any,
+    handleBlur: any,
+    values: any,
+    errors: any,
+    touched: any,
+   ) => (
+    <LabelAndFeilds index={index} label={label} handleChange={handleChange} handleBlur={handleBlur} values={values} errors={errors} touched={touched} type="password"/>
+  ),
+  number: (
+    index: number,
+    label: string,
+    handleChange: any,
+    handleBlur: any,
+    values: any,
+    errors: any,
+    touched: any,
+  ) => (
+        <LabelAndFeilds index={index} label={label} handleChange={handleChange} handleBlur={handleBlur} values={values} errors={errors} touched={touched} type="number"/>
+  ),
+  text: (
+    index: number,
+    label: string,
+    handleChange: any,
+    handleBlur: any,
+    values: any,
+    errors: any,
+    touched: any,
+  ) => (
+           <LabelAndFeilds index={index} label={label} handleChange={handleChange} handleBlur={handleBlur} values={values} errors={errors} touched={touched} type="text"/>
+  ),
+  button: (
+    index: any,
+    label: any,
+    handleChange: any,
+    handleBlur: any,
+    values: any,
+    errors: any,
+    touched: any,
+    action: any,
+    ) => (
+    <div id={`${index}`}>
+    <Box sx={{mb:2, justifyContent:'space-between',display:'flex' }}>
+      <Button type={action} variant="contained" size="small">{action}</Button>
+      <Button type='button' color='error' size="small" onClick={() => { document.getElementById(`${index}`)!.remove();}} >❌</Button>
+    </Box>
+    </div>
+  ),
+};
 
 const FORM_BUILDER = [
-  { id: 1, type: "email", label: "Please Enter Email" },
-  { id: 2, type: "password", label: "Please Enter Password" },
-  { id: 3, type: "text", label: "Please Enter text" },
+  { id: 1, type: "email", label: "Please Enter Email",validate:true },
+  { id: 2, type: "password", label: "Please Enter Password",validate:true },
+  { id: 3, type: "text", label: "Please Enter text" ,validate:true},
   { id: 4, type: "number", label: "Please Enter number" },
   { id: 5, type: "button", action: "submit", label: "submit the form" },
   { id: 6, type: "button", action: "reset", label: "reset the form" },
 ];
 
-export const Basket = ({ setFormBuilder }: any) => {
-  const [basket, setBasket] = useState([]);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Basket = () => {
+  const [formBuilder, setFormBuilder] = useState<any>([]);
+  const[isValidating,setValidating]= useState<any>([]);
+  const [isDragging,setDragging]=useState<Boolean>(false);
+
   const [{ isOver }, dropRef] = useDrop({
     accept: "form-builder",
     drop: (item) => {
-      setBasket((basket: any) =>
-        !basket.includes(item) ? [...basket, item] : basket
-      );
       setFormBuilder((basket: any) =>
         !basket.includes(item) ? [...basket, item] : basket
       );
@@ -29,31 +95,87 @@ export const Basket = ({ setFormBuilder }: any) => {
       isOver: monitor.isOver(),
     }),
   });
+  const initialValues = useMemo(() => {
+    const obj = Object.fromEntries(
+      formBuilder.map((data: any, index: number) => {
+        setDragging(true);
+        console.log("data is ", data);
+        // if(data?.validate){
+        //   const test={}
+        //   console.log("test = ",test);
+        //   setValidating([...isValidating,test]);
+        // }
+        if (data?.type !== "button") {
+          return [`${data.type}_${index}`, ""];
+        }
+        return [`submit`, ""];
+      })
+    );
+    return Object.fromEntries(
+      Object.entries(obj).filter(([key]) => !key.includes("submit"))
+    );
+  }, [formBuilder]);
+
+  console.log("IntialValues = ", initialValues);
+  console.log("validating = ",isValidating);
+  // const schema = yup.object().shape({});
+  
 
   return (
-    <React.Fragment>
-      <div className="card">
-        {FORM_BUILDER.map((form) => (
-          <Card
-            draggable
-            id={form.id}
-            type={form.type}
-            label={form.label}
-            action={form.action}
-          />
-        ))}
+    <Box sx={{display:'flex'}}>
+      <Box sx={{backgroundColor:'grey',width:150,minHeight:'50vh',display:'grid',justifyContent:'space-between'}}>
+            {FORM_BUILDER.map((form) => (
+              <Card
+                draggable
+                id={form.id}
+                type={form.type}
+                label={form.label}
+                action={form.action}
+                // validate={form.validate} 
+                />
+            ))}
+      </Box>
+      <Box>
+      <div ref={dropRef}>
+        
+      <Formik
+        initialValues={initialValues}
+        // validate={validate(schema)}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log("Submitted");
+          setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting
+        }: any) => (  
+          <form onSubmit={handleSubmit} className="formData">
+            {formBuilder.map((data: any, index: number) => (
+              <>
+                {inputObject[data?.type](
+                  index,
+                  data?.label,
+                  handleChange,
+                  handleBlur,
+                  values,
+                  errors,
+                  touched,
+                  data?.action
+                )}
+                
+              </>
+            ))}
+          {!isDragging &&
+          <Box sx={{textAlign:'center',alignItems:'center',marginTop:'30%'}}>Drop item here from left side</Box>
+          }
+          </form>
+        )}
+      </Formik>
       </div>
-      <div className="basket" ref={dropRef}>
-        {basket.map((form: any) => (
-          <Card
-            id={form.id}
-            type={form.type}
-            label={form.label}
-            action={form.action}
-          />
-        ))}
-        {<div className="mt-12">Drop Here!</div>}
-      </div>
-    </React.Fragment>
+      </Box>
+      </Box>
   );
 };
