2010199945617d02483f27796e1651f9f32fa992
Jay Salot <jay.salot@bitontree.com>
2023-01-03 17:45:23 +0530

    Build fix

--- src/card.tsx
+++ src/card.tsx
@@ -1,24 +1,21 @@
-import { Box} from "@mui/material";
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useDrag } from "react-dnd";
 
-export const Card = ({ id, type, action,validate }: any) => {
+export const Card = ({ id, type, action, validate }: any) => {
   const [{ isDragging }, dragRef] = useDrag({
     type: "form-builder",
-    item: {type, action,validate ,id,},
+    item: { type, action, validate, id },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
   });
 
-
   return (
-     <>
-    <div ref={dragRef} className="dropItemsPanelData"> 
-       <div className="dropItems">
-        {action ?? type}
-       </div>
-      {/* {isDragging} */}
-    </div>
-   </>
+    <>
+      <div ref={dragRef} className="dropItemsPanelData">
+        <div className="dropItems">{action ?? type}</div>
+        {/* {isDragging} */}
+      </div>
+    </>
   );
 };
